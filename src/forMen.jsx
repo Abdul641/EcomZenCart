3c129c3bb3a3730e6cf39633f6e4f5ef9f91aabe
AbdulRehman <111703045+Abdul641@users.noreply.github.com>
2024-03-31 17:58:54 -0400

    Men section top picks done

--- src/forMen.jsx
+++ src/forMen.jsx
@@ -15,9 +15,25 @@ export function ForMen() {
           </div>
         </div>
 
-        {/*  */}
-        <div className="forMenImg-div"></div>
-        <div className="forMenImg-div"></div>
+        {/* Ripped Jeans */}
+        <div className="mainRipped-div">
+          <div className="RippedJeans-div"></div>
+          <div className="TravisScott-Content">
+            <h1 className="AirJordans">Men Stacked Ripped Jeans</h1>
+            <hr></hr>
+            <h2 className="AirJordans-description">Cool Slim Fit</h2>
+          </div>
+        </div>
+
+        {/* Puffer Jacket */}
+        <div className="mainJacket-div">
+          <div className="PufferJacket-div"></div>
+          <div className="TravisScott-Content">
+            <h1 className="AirJordans">Men Hooded Puffer Jacket</h1>
+            <hr></hr>
+            <h2 className="AirJordans-description">Coat & Jackets</h2>
+          </div>
+        </div>
       </div>
     </>
   );
